a733bca54491599fd9e7d31452b20c638f176f8c
SebastianGG20211 <94941148+SebastianGG20211@users.noreply.github.com>
2023-01-31 20:22:58 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+video = "";
+
+function preload0(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas (480, 380);
+    canvas.center();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Estado : Detectado objetos";
+
+}
+
+function modelLoaded() {
+    console.log("¡Modelo cargando!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
